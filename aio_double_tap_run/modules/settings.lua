6fd623ac77542f43301ba70aaf9b80b01dfa0620
Danny G. <69323643+degiel1982@users.noreply.github.com>
2025-03-23 04:12:13 +0000

    Added the modules

---
+++ aio_double_tap_run/modules/settings.lua
@@ -0,0 +1,10 @@
+return {
+    normal_speed = 1.0,  -- Default speed when not sprinting.
+    sprint_speed = 1.5,  -- Speed when sprinting.
+    -- Double tap configuration
+    double_tap_run = {
+        interval       = 0.5,  -- Time window for double tap (seconds)
+        liquid_update_interval = 0.5,  -- Time window for liquid check (seconds)
+    }
+
+}
