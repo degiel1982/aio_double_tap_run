f96e2b9dfca462e30c9a9069bb1274cc113443fc
Danny G. <69323643+degiel1982@users.noreply.github.com>
2025-03-24 00:43:52 +0000

    update

--- aio_double_tap_run/tap_control_per_mod/tenplus1_tap.lua
+++ aio_double_tap_run/tap_control_per_mod/tenplus1_tap.lua
@@ -3,10 +3,12 @@ local update_double_tap = dofile(core.get_modpath("aio_double_tap_run").."/modul
 local tenplus1s = {
     get_saturation = dofile(core.get_modpath("aio_double_tap_run").."/modules/get_stamina_tenplus1s.lua")	
 }
+
 local player_double_tap = {}
 local player_is_sprinting = {}
 local settings = {
-    stamina_drain = core.settings:get("stamina_sprint_drain") or 0.35,
+    stamina_drain_sprint = core.settings:get("stamina_sprint_drain") or 0.35,
+    extra_speed = core.settings:get("stamina_sprint_speed") or 0.3,
 }
 core.register_on_leaveplayer(function(player)
     local name = player:get_player_name()
@@ -35,20 +37,28 @@ core.register_globalstep(function(dtime)
             sprinting = false
         end
 
+      
+
         if sprinting then
             local current_stamina = tenplus1s.get_saturation(player)
             if current_stamina >= 7 then
                 if not player_is_sprinting[name] then
-                    player:set_physics_override({ speed = 1.5 }) 
+                    player:set_physics_override({ speed = (1.2 + settings.extra_speed) })
                     player_is_sprinting[name] = true
                 end
-                stamina.change(player, -0.5 * dtime)
+                stamina.change(player, -(settings.stamina_drain_sprint / (current_stamina / 8)) * dtime)
             else
                 if player_is_sprinting[name] then
-                player:set_physics_override({ speed = 1 }) 
-                player_is_sprinting[name] = false
+                    player:set_physics_override({ speed = 1 })
+                    player_is_sprinting[name] = false
                 end
             end
+        else
+            if player_is_sprinting[name] then
+                player:set_physics_override({ speed = 1 })
+                player_is_sprinting[name] = false
+            end
         end
+
     end
 end)
