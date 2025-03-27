3bbef1764cc7b26fd8c29a02851cf9f8060ead08
Danny G. <69323643+degiel1982@users.noreply.github.com>
2025-03-27 10:54:59 +0100

    Update tools.lua

--- modules/tools.lua
+++ modules/tools.lua
@@ -16,4 +16,39 @@ local function player_is_in_liquid(pos)
     return false
 end
 
-return player_is_in_liquid
+
+local function dt_sensor(dt_state, dtime, key_pressed, trigger_delay)
+    if not dt_state.sprinting then
+        if dt_state.count > 0 then
+            dt_state.timer = dt_state.timer + dtime
+        end
+
+        if key_pressed and not dt_state.was_up then
+            dt_state.count = dt_state.count + 1
+            if dt_state.count == 1 then
+                dt_state.timer = 0
+            end
+        end
+
+        dt_state.was_up = key_pressed
+
+        if dt_state.timer > trigger_delay then
+            dt_state.count = 0
+            dt_state.timer = 0
+        end
+
+        if dt_state.count == 2 and dt_state.timer <= trigger_delay then
+            dt_state.sprinting = true
+        end
+    else
+        if not key_pressed then
+            dt_state.sprinting = false
+            dt_state.count = 0
+            dt_state.timer = 0
+        end
+    end
+
+    return dt_state.sprinting
+end
+
+return player_is_in_liquid, dt_sensor
