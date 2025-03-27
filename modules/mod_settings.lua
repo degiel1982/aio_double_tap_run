77460d2cd32705b53ad1e73f4e79dcd00faf66e3
Danny G. <69323643+degiel1982@users.noreply.github.com>
2025-03-27 11:07:12 +0000

    update

--- modules/mod_settings.lua
+++ modules/mod_settings.lua
@@ -1,3 +1,4 @@
+local player_is_in_liquid, dt_sensor, sprint, get_mod_author = dofile(core.get_modpath("aio_double_tap_run").."/modules/tools.lua")
 local stamina_is_installed = core.get_modpath("stamina") ~= nil
 local hunger_is_installed = core.get_modpath("hunger_ng") ~= nil
 local settings = {}
@@ -5,13 +6,17 @@ local settings = {}
 
 
 if stamina_is_installed then
-    settings = {
-        trigger_delay = 0.5,
-        stamina_sprint_drain = tonumber(core.settings:get("stamina_sprint_drain")) or 0.35,
-        move_exhaust = 1.5,
-        enable_stamina = core.settings:get_bool("enable_stamina", true),
-        treshold = 6
-    }
+    local mod_author = get_mod_author("stamina")
+    if mod_author == "TenPlus1" then
+        settings = {
+
+            trigger_delay = 0.5,
+            stamina_sprint_drain = tonumber(core.settings:get("stamina_sprint_drain")) or 0.35,
+            move_exhaust = 1.5,
+            enable_stamina = core.settings:get_bool("enable_stamina", true),
+            treshold = 6
+        }
+    end
 elseif hunger_is_installed then
 
 end
