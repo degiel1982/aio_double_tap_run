65e307accb4c64903f673a4800a5c3c101782f8e
Danny G. <69323643+degiel1982@users.noreply.github.com>
2025-03-31 19:33:11 +0200

    update

--- init.lua
+++ init.lua
@@ -1,7 +1,55 @@
 local set_sprinting, mod_settings = dofile(core.get_modpath("aio_double_tap_run") .. "/functions/physics.lua")
-    
+local player_double_tap = {}
 aio_double_tap_run = {}
 aio_double_tap_run.set_sprinting = set_sprinting
+
+local function cancel_run(p_pos, player)
+    local cancel_run = false
+    local name = player:get_player_name()
+    --when in liquid
+    if mod_settings.tools.player_is_in_liquid(p_pos,player) then
+        cancel_run = true
+    end
+    --when starving
+    if (mod_settings.stamina.sofar.installed or mod_settings.stamina.tenplus.installed) and mod_settings.stamina_drain then
+        if mod_settings.stamina.sofar.installed then
+            player_double_tap[name].starving = mod_settings.tools.is_player_starving(
+                stamina.get_saturation(player),
+                mod_settings.stamina.sofar.treshold * 2
+            )
+        end
+        if mod_settings.stamina.tenplus.installed then
+            player_double_tap[name].starving = mod_settings.tools.is_player_starving(
+                stamina.get_saturation(player),
+                mod_settings.stamina.tenplus.treshold
+            )
+        end
+
+        if player_double_tap[name].starving then
+            cancel_run = true
+        end
+    end
+    if mod_settings.hunger_ng.installed then
+        local info = hunger_ng.get_hunger_information(name)
+        if not info.invalid then
+            -- Cancel sprinting if hunger is below the threshold
+            if info.hunger.exact <= mod_settings.hunger_ng.treshold then
+                if player_double_tap[name].running then
+                    player_double_tap[name].running = false
+                end
+            end
+        end
+    end
+    -- If the player is standing on a ladder, cancel sprinting
+    if mod_settings.tools.is_player_on_ladder(player) and not mod_settings.ladder_sprint then
+        cancel_run = true
+    end
+
+    if mod_settings.tools.is_player_running_against_wall(player) then
+        cancel_run = true
+    end
+    return cancel_run
+end
 
 core.register_on_mods_loaded(function()
     local has_beds = minetest.get_modpath("beds") ~= nil
@@ -57,7 +105,7 @@ local function show_sprint_particles(player)
         })
     end
 end
-local player_double_tap = {}
+
 
 local player_data = {
     count = 0,
@@ -84,15 +132,9 @@ core.register_globalstep(function(dtime)
             player_double_tap[name] = player_data
         end
 
-        if not mod_settings.liquid_sprint then
-            player_double_tap[name].wet = mod_settings.tools.player_is_in_liquid(p_pos)
-        end
-
-           
-        
         local pos = player:get_pos()
-    
-        if not player_double_tap[name].wet then
+
+        if cancel_run(pos, player) == false then
             local control_bits = player:get_player_control_bits()
             local key_is_pressed = control_bits == 1 or control_bits == 17
             if mod_settings.use_dt then
@@ -111,92 +153,47 @@ core.register_globalstep(function(dtime)
                     player_double_tap[name].running = key_press
                 end
             end
-        else
-            player_double_tap[name].running = false
-        end
-        if mod_settings.hunger_ng.installed then
-            local info = hunger_ng.get_hunger_information(name)
-            if not info.invalid then
-                -- Cancel sprinting if hunger is below the threshold
-                if info.hunger.exact <= mod_settings.hunger_ng.treshold then
-                    if player_double_tap[name].running then
-                        player_double_tap[name].running = false
+            if player_double_tap[name].running then
+                set_sprinting(player, true)
+                if not mod_settings.stamina.sofar.installed and not mod_settings.stamina.tenplus.installed and mod_settings.enable_particles then
+                    show_sprint_particles(player)
+                end
+                if mod_settings.stamina.sofar.installed and mod_settings.stamina_drain then
+                    stamina.exhaust_player(player, mod_settings.stamina.sofar.exhaust_sprint * dtime)
+                end
+                if mod_settings.stamina.tenplus.installed and mod_settings.stamina_drain then
+                    stamina.exhaust_player(player, (mod_settings.stamina.tenplus.exhaust_sprint * 100) * dtime)
+                end
+                if mod_settings.hunger_ng.installed then
+                    hunger_ng.alter_hunger(name, -mod_settings.hunger_ng.exhaust_sprint * dtime, 'Sprinting') 
+                end
+                if mod_settings.enable_animation and mod_settings.character_anim then
+                    local current_animation = player:get_animation()
+                    local animation_range = current_animation and { x = current_animation.x, y = current_animation.y } or { x = 0, y = 79 }
+                    local sprint_speed = mod_settings.sprint_framespeed + ((player:get_velocity().x^2 + player:get_velocity().z^2)^0.5 * 2)
+                    player:set_animation(animation_range, sprint_speed, 0)
+                end
+            else
+                if not mod_settings.stamina.tenplus.installed then
+                    if mod_settings.pova.installed or (mod_settings.pova.installed == false and mod_settings.player_monoids.installed == false) then
+                        set_sprinting(player, false)
+                        if mod_settings.stamina.sofar.installed and mod_settings.stamina_drain then
+                            stamina.exhaust_player(player, mod_settings.stamina.sofar.exhaust_move * dtime)
+                        end
+                        if mod_settings.stamina.tenplus.installed and mod_settings.stamina_drain then
+                            stamina.exhaust_player(player, mod_settings.stamina.tenplus.exhaust_move * dtime)
+                        end
                     end
                 end
-            end
-        end
-
-        if (mod_settings.stamina.sofar.installed or mod_settings.stamina.tenplus.installed) and mod_settings.stamina_drain then
-            if mod_settings.stamina.sofar.installed then
-                player_double_tap[name].starving = mod_settings.tools.is_player_starving(
-                    stamina.get_saturation(player),
-                    mod_settings.stamina.sofar.treshold * 2
-                )
-            end
-            if mod_settings.stamina.tenplus.installed then
-                player_double_tap[name].starving = mod_settings.tools.is_player_starving(
-                    stamina.get_saturation(player),
-                    mod_settings.stamina.tenplus.treshold
-                )
-            end
-
-            if player_double_tap[name].starving then
-                player_double_tap[name].running = false
-            end
-        end
-    
-        -- If the player is standing on a ladder, cancel sprinting
-        if mod_settings.tools.is_player_on_ladder(player) and not mod_settings.ladder_sprint then
-            player_double_tap[name].running = false
-            --set_sprinting(player, false)
-        end
-
-        if mod_settings.tools.is_player_running_against_wall(player) then
-            player_double_tap[name].running = false
-           
-        end
-        --if mod_settings.tools.is_player_flying_or_over_air(player) then
-        --    player_double_tap[name].running = false
-        --   
-        --end
-        if player_double_tap[name].running then
-            set_sprinting(player, true)
-            if not mod_settings.stamina.sofar.installed and not mod_settings.stamina.tenplus.installed and mod_settings.enable_particles then
-                show_sprint_particles(player)
-            end
-            if mod_settings.stamina.sofar.installed and mod_settings.stamina_drain then
-                stamina.exhaust_player(player, mod_settings.stamina.sofar.exhaust_sprint * dtime)
-            end
-            if mod_settings.stamina.tenplus.installed and mod_settings.stamina_drain then
-                stamina.exhaust_player(player, (mod_settings.stamina.tenplus.exhaust_sprint * 100) * dtime)
-            end
-            if mod_settings.hunger_ng.installed then
-                hunger_ng.alter_hunger(name, -mod_settings.hunger_ng.exhaust_sprint * dtime, 'Sprinting') 
-            end
-            local current_animation = player:get_animation()
-            local animation_range = current_animation and { x = current_animation.x, y = current_animation.y } or { x = 0, y = 79 }
-            local sprint_speed = mod_settings.sprint_framespeed + ((player:get_velocity().x^2 + player:get_velocity().z^2)^0.5 * 2)
-            if mod_settings.enable_animation then
-                player:set_animation(animation_range, sprint_speed, 0)
-            end
-        else
-            if not mod_settings.stamina.tenplus.installed then
-                if mod_settings.pova.installed or (mod_settings.pova.installed == false and mod_settings.player_monoids.installed == false) then
-                    set_sprinting(player, false)
-                    if mod_settings.stamina.sofar.installed and mod_settings.stamina_drain then
-                        stamina.exhaust_player(player, mod_settings.stamina.sofar.exhaust_move * dtime)
-                    end
-                    if mod_settings.stamina.tenplus.installed and mod_settings.stamina_drain then
-                        stamina.exhaust_player(player, mod_settings.stamina.tenplus.exhaust_move * dtime)
-                    end
+                if mod_settings.enable_animation and mod_settings.character_anim then
+                    local current_animation = player:get_animation()
+                    local animation_range = current_animation and { x = current_animation.x, y = current_animation.y } or { x = 0, y = 79 }
+                    local sprint_speed = mod_settings.walk_framespeed + ((player:get_velocity().x^2 + player:get_velocity().z^2)^0.5 * 2)
+                    player:set_animation(animation_range, sprint_speed, 0)
                 end
             end
-            local current_animation = player:get_animation()
-            local animation_range = current_animation and { x = current_animation.x, y = current_animation.y } or { x = 0, y = 79 }
-            local sprint_speed = mod_settings.walk_framespeed + ((player:get_velocity().x^2 + player:get_velocity().z^2)^0.5 * 2)
-            if mod_settings.enable_animation then
-                player:set_animation(animation_range, sprint_speed, 0) -- Running animation
-            end
+        else
+            player_double_tap[name].running = false
         end
     end
 end)
