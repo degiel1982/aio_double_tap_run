54ffccb42b384ff4123e80df4a8354f780549baf
Danny G. <69323643+degiel1982@users.noreply.github.com>
2025-04-01 11:01:13 +0200

    Maintained the code

--- init.lua
+++ init.lua
@@ -3,57 +3,75 @@ local player_double_tap = {}
 aio_double_tap_run = {}
 aio_double_tap_run.set_sprinting = set_sprinting
 
-local function cancel_run(p_pos, player)
+local cancel_timer_timestamps = {}
+local function check_timer_interval(player_name, interval)
+    if not cancel_timer_timestamps[player_name] then
+        cancel_timer_timestamps[player_name] = 0
+    end
+    local current_time = minetest.get_us_time() / 1000000  -- Get current time in seconds.
+    local last_time = cancel_timer_timestamps[player_name]
+    
+    if not last_time or (current_time - last_time <= interval) then
+        cancel_timer_timestamps[player_name] = current_time
+        return true
+    end
+
+    return false
+end
+
+local function cancel_run(p_pos, player, interval_is_true)
     local name = player:get_player_name()
     local test = false
-    --[[
-        LIQUID CHECK
-    ]]
-    if mod_settings.tools.player_is_in_liquid(p_pos,player) then
-        test = true
-    end
-    --[[
-        STARVE CHECK
-    ]]
-    -- When Stamina Mod is installed
-    local stamina_treshold = mod_settings.stamina.sofar.treshold * 2
-    if (mod_settings.stamina.sofar.installed or mod_settings.stamina.tenplus.installed) and mod_settings.stamina_drain then
-        local treshold = 0
-        local curent_saturation = stamina.get_saturation(player)
-        if mod_settings.stamina.sofar.installed then
-            treshold = stamina_treshold
-        end
-        if mod_settings.stamina.tenplus.installed then
-            treshold = mod_settings.stamina.tenplus.treshold
-
-        end
-        player_double_tap[name].starving = mod_settings.tools.is_player_starving(curent_saturation, treshold)
-        if player_double_tap[name].starving then
-            player_double_tap[name].running = false
+    if not interval_is_true then
+        --[[
+            LIQUID CHECK
+        ]]
+        if mod_settings.tools.player_is_in_liquid(p_pos,player) then
             test = true
-        end        
-    end
-    -- When hunger_ng mod is installed
-    if mod_settings.hunger_ng.installed then
-        local info = hunger_ng.get_hunger_information(name)
-        if not info.invalid then
-            if info.hunger.exact <= mod_settings.hunger_ng.treshold then
+        end
+        --[[
+            STARVE CHECK
+        ]]
+        -- When Stamina Mod is installed
+        local stamina_treshold = mod_settings.stamina.sofar.treshold * 2
+        if (mod_settings.stamina.sofar.installed or mod_settings.stamina.tenplus.installed) and mod_settings.stamina_drain then
+            local treshold = 0
+            local curent_saturation = stamina.get_saturation(player)
+            if mod_settings.stamina.sofar.installed then
+                treshold = stamina_treshold
+            end
+            if mod_settings.stamina.tenplus.installed then
+                treshold = mod_settings.stamina.tenplus.treshold
+
+            end
+            player_double_tap[name].starving = mod_settings.tools.is_player_starving(curent_saturation, treshold)
+            if player_double_tap[name].starving then
                 player_double_tap[name].running = false
                 test = true
+            end        
+        end
+        -- When hunger_ng mod is installed
+        if mod_settings.hunger_ng.installed then
+            local info = hunger_ng.get_hunger_information(name)
+            if not info.invalid then
+                if info.hunger.exact <= mod_settings.hunger_ng.treshold then
+                    player_double_tap[name].running = false
+                    test = true
+                end
             end
         end
-    end
-    --[[
-        LADDER CHECK
-    ]]
-    if mod_settings.tools.is_player_on_ladder(player) and not mod_settings.ladder_sprint then
-        test = true
-    end
-    --[[
-        WALL CHECK
-    ]]
-    if mod_settings.tools.is_player_running_against_wall(player) then
-        test = true
+        --[[
+            LADDER CHECK
+        ]]
+        if mod_settings.tools.is_player_on_ladder(player) and not mod_settings.ladder_sprint then
+            test = true
+        end
+        --[[
+            WALL CHECK
+        ]]
+        if mod_settings.tools.is_player_running_against_wall(player) then
+            test = true
+        end
     end
     --[[
         RETURN VALUE: FALSE
@@ -126,8 +144,8 @@ core.register_globalstep(function(dtime)
         end
 
         local pos = player:get_pos()
-
-        if cancel_run(pos, player) == false then
+        local icheck = check_timer_interval(name, 0.5)
+        if cancel_run(pos, player, icheck) == false and not player_double_tap[name].running then
             local control_bits = player:get_player_control_bits()
             local key_is_pressed = control_bits == 1 or control_bits == 17 or control_bits == 513
             if mod_settings.use_dt then
