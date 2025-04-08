6dcafd31ad4199398abbaffd52b72791d75c7fc0
Danny G. <69323643+degiel1982@users.noreply.github.com>
2025-04-08 15:10:11 +0200

    Fixed Bug: draining when disabled

--- api_modules/hunger.lua
+++ api_modules/hunger.lua
@@ -1,5 +1,6 @@
 local hunger_ng_treshold = tonumber(core.settings:get("aio_double_tap_run.hunger_treshold")) or 6
 local ENABLE_STARVE =  core.settings:get_bool("aio_double_tap_run.starve_check", true)
+local ENABLE_DRAIN =  core.settings:get_bool("aio_double_tap_run.enable_stamina_drain", true)
 
 -- Register callback for double-tap detection
 aio_double_tap_run.register_dt_data_callback(function(player, filtered_data, dtime)
@@ -13,7 +14,9 @@ aio_double_tap_run.register_dt_data_callback(function(player, filtered_data, dti
             end
         end
         if filtered_data.dt_detected then
-            hunger_ng.alter_hunger(player_name, -(0.5 * dtime), 'Sprinting') 
+            if ENABLE_DRAIN then
+                hunger_ng.alter_hunger(player_name, -(0.5 * dtime), 'Sprinting')
+            end 
         end
     end
 end)
