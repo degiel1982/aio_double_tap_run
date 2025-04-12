d9effaee080c3fe87dc371c332673b8fcdddd11f
Danny G. <69323643+degiel1982@users.noreply.github.com>
2025-04-12 20:27:29 +0200

    Update hunger_ng.lua

--- addons/hunger_ng.lua
+++ addons/hunger_ng.lua
@@ -9,14 +9,17 @@ local functions = dofile(core.get_modpath(mod_name) .. "/core/functions.lua")
 aio_double_tap_run.register_callback(function(player, data, dtime)
     if not aio_double_tap_run.is_player(player) then return nil end
     local player_name = player:get_player_name()
-    local info = hunger_ng.get_hunger_information(player_name)
-    if info.hunger.exact <= hunger_ng_treshold then
-        data.cancel_sprint = true
-        return data
+    if ENABLE_STARVE then
+        local info = hunger_ng.get_hunger_information(player_name)
+        if info.hunger.exact <= hunger_ng_treshold then
+            data.cancel_sprint = true
+            return data
+        end
     end
-
-    if data.is_sprinting and not functions.in_air(player,1) and not functions.wall_bump(player) then
-        hunger_ng.alter_hunger(player_name, -(0.5 * dtime), 'Sprinting')
+    if ENABLE_DRAIN then
+        if data.is_sprinting and not functions.in_air(player,1) and not functions.wall_bump(player) then
+            hunger_ng.alter_hunger(player_name, -(0.5 * dtime), 'Sprinting')
+        end
     end
     return nil
 end)
